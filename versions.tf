13f1d1698267366311b8083dfe89ab9a530d4623
Yuri Kilian <kilian.inf@gmail.com>
2021-12-08 17:20:43 +0000

    Initial commit
    Signed-off-by: Yuri Kilian <kilian.inf@gmail.com>

---
+++ versions.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    kind = {
+      source  = "kyma-incubator/kind"
+      version = "0.0.9"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.5.0"
+    }
+    
+    null = {
+      source  = "hashicorp/null"
+      version = "3.1.0"
+    }
+  }
+
+  required_version = ">= 1.0.0"
+}
